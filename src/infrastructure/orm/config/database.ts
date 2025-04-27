6124b33d4d1776caeecce77ad91db4ee7acfe1c4
Ricardo Reyes <reyesramirezricardoemanuel@gmail.com>
2025-04-27 15:56:01 -0500

    Fix availabel port for database

--- src/infrastructure/orm/config/database.ts
+++ src/infrastructure/orm/config/database.ts
@@ -10,7 +10,7 @@ const getBaseConfig = (env: string): Partial<DataSourceOptions> => {
   return {
     type: 'postgres' as const,
     host: process.env.DB_HOST || 'localhost',
-    port: parseInt(process.env.DB_PORT || '5432'),
+    port: parseInt(process.env.DB_PORT || '5433'),
     username: process.env.DB_USERNAME || 'postgres',
     password: process.env.DB_PASSWORD || 'postgres',
     entities,
